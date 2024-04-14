717bc095ef00d955f75c0b770028a308ff009137
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-14 16:03:44 +0200

    Routing and Navigiations

---
+++ src/components/eCommerce/index.ts
@@ -0,0 +1,5 @@
+import HeaderBasket from "./headerBasket/HeaderBasket";
+import Category from "./category/Category";
+import Product from "./product/Product";
+
+export { HeaderBasket, Product, Category };
---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,63 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// layouts
+import MainLayout from "@/templates/MainLayout/MainLayout";
+// pages
+import Home from "@/views/Home";
+import Categories from "@/views/Categories";
+import Products from "@/views/Products";
+import AboutUs from "@/views/AboutUs";
+import Login from "@/views/Login";
+import Register from "@/views/Register";
+import Error from "@/views/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "categories",
+        element: <Categories />,
+      },
+      {
+        path: "products/:prefix",
+        element: <Products />,
+        loader: ({ params }) => {
+          if (
+            typeof params.prefix !== "string" ||
+            !/^[a-z]+$/i.test(params.prefix)
+          ) {
+            throw new Response("Bad Request", {
+              statusText: "Category not found",
+              status: 400,
+            });
+          }
+          return true;
+        },
+      },
+      {
+        path: "about-us",
+        element: <AboutUs />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const AppRouter = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
