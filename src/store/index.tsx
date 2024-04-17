7c891b08d4e4e754b194d0683e860020aad9652f
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-17 02:28:09 +0200

    ignoredActions in serializableCheck for redux persist

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,27 +1,48 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import categoriesReducer from "./categories/categoriesSlice";
 import productsReducer from "./products/productsSlice";
 import cartReducer from "./cart/cartSlice";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 
-const rootPersistConfig = {
+/*const rootPersistConfig = {
   key: "root",
   storage,
   whiteList: ["cart"],
+};*/
+
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+  whiteList: ["items"],
 };
 
 const rootReducer = combineReducers({
   categories: categoriesReducer,
   products: productsReducer,
-  cart: cartReducer,
+  cart: persistReducer(cartPersistConfig, cartReducer),
 });
 
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+//const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  // reducer: persistedReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistedStore = persistStore(store);
