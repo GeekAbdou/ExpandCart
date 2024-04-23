b8ec273fc81a8fb26f65a6105d521ddae23f8eb2
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-23 03:49:54 +0200

    removing components file

--- src/components/shared/Heading/Heading.tsx
+++
@@ -1,9 +0,0 @@
-function Heading({ title }: { title: string }) {
-  return (
-    <div className="mb-3" style={{ fontSize: "26px" }}>
-      {title}
-    </div>
-  );
-}
-
-export default Heading;
--- src/components/shared/index.ts
+++
@@ -1,8 +0,0 @@
-import Header from "@/components/shared/Header/Header";
-import Footer from "@/components/shared/Footer/Footer";
-import Heading from "@/components/shared/Heading/Heading";
-import GridList from "@/components/shared/GridList/GridList";
-import Loader from "@/components/shared/Loader/Loader";
-import PageSpinner from "@/components/shared/Loader/PageSpinner";
-
-export { Header, Footer, Heading, GridList, Loader, PageSpinner };
